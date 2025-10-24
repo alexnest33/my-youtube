2afb08a173f4aae460cadd5ffe1669748a14b651
Aliaksandr nest <aliaksandrnest@Air-Aliaksandr.bbrouter>
2025-10-24 13:03:48 +0300

    ready

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,18 @@
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router";
+import { PersistGate } from "redux-persist/integration/react";
 import App from "./App.jsx";
-import store from "./redux/store/store.js";
+import store, { persistor } from "./redux/store/store.js";
+
 
 createRoot(document.getElementById("root")).render(
   <BrowserRouter>
     <Provider store={store}>
-      <App />
+      <PersistGate loading={null} persistor={persistor} >
+        <App />
+      </PersistGate>
     </Provider>
   </BrowserRouter>
 );
+  
--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,14 +1,37 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { combineReducers, configureStore } from "@reduxjs/toolkit";
+import { persistStore, persistReducer, FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER} from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import authSlice from "../slices/authSlice";
 import contentSlice from "../slices/contentSlice";
 import saveInfoSlice from "../slices/saveInfoSlice";
 
-const store = configureStore({
-  reducer: {
-    login: authSlice,
-    content: contentSlice,
-    saveInfo: saveInfoSlice,
-  },
+const persistConfig = {
+  key: "root",
+  storage,
+};
+
+const rootReducer = combineReducers({
+  login: authSlice,
+  content: contentSlice,
+  saveInfo: saveInfoSlice,
 });
 
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
 export default store;
