003b9094844a02cd2dc1af742b4c9b61a2e9d6da
Aliaksandr nest <aliaksandrnest@Air-Aliaksandr.bbrouter>
2025-10-28 18:07:47 +0300

    blabla

--- src/components/VideoGallery/index.jsx
+++ src/components/VideoGallery/index.jsx
@@ -6,15 +6,11 @@ import ViewMode from "../ViewMode";
 import NoResults from "../NoResults";
 
 const VideoGallery = () => {
+  
   const [viewMode, setViewMode] = useState("list");
-
   const { items } = useSelector((store) => store.content);
   const { active } = useSelector((store) => store.saveInfo);
-  console.log('API Key:', import.meta.env.VITE_YOUTUBE_API_KEY);
-  console.log('YouTube URL:', import.meta.env.VITE_YOUTUBE_ID);
-  console.log('YouTube Query:', import.meta.env.VITE_YOUTUBE_QUERY);
-  console.log('Production items:', items);
-  console.log('First item structure:', items?.[0])
+ 
 
 
   const dispatch = useDispatch();
